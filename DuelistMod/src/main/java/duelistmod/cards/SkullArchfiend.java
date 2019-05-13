31a4be5f58b91cb8a19100f16b55615e4e5aa250
Adam Bennett <nyoxidetwitter@gmail.com>
2019-05-13 00:08:42 -0400

    syncing increment keyword json fix

--- DuelistMod/src/main/java/duelistmod/cards/SkullArchfiend.java
+++ DuelistMod/src/main/java/duelistmod/cards/SkullArchfiend.java
@@ -58,7 +58,7 @@ public class SkullArchfiend extends DuelistCard
 			if (c instanceof DuelistCard)
 			{
 				DuelistCard dC = (DuelistCard)c;
-				dC.modifyTributes(this.magicNumber);
+				if (dC.tributes > 0) { dC.modifyTributes(this.magicNumber); }
 			}
 			AbstractDungeon.actionManager.addToTop(new ModifyDamageAction(c.uuid, this.magicNumber * 6));
 		}
