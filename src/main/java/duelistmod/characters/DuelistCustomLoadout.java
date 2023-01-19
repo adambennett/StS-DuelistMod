4b417b27376c46377fa15a422e28de4132515156
May Lau <ascriptmaster@gmail.com>
2023-01-19 00:16:11 -0800

    Add new Duelist deck images for character select

--- src/main/java/duelistmod/characters/DuelistCustomLoadout.java
+++ src/main/java/duelistmod/characters/DuelistCustomLoadout.java
@@ -57,43 +57,27 @@ public class DuelistCustomLoadout
 		}
 
 		if (selectScreen != null) {
-			// Standard
-			if (DuelistMod.deckIndex == 0) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Dragon
-			if (DuelistMod.deckIndex == 1) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Nature
-			else if (DuelistMod.deckIndex == 2) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Spellcaster
-			else if (DuelistMod.deckIndex == 3) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Toon
-			else if (DuelistMod.deckIndex == 4) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Zombie
-			else if (DuelistMod.deckIndex == 5) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Aqua
-			else if (DuelistMod.deckIndex == 6) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Fiend
-			else if (DuelistMod.deckIndex == 7) {  selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Machine
-			else if (DuelistMod.deckIndex == 8) { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Superheavy
-			else if (DuelistMod.deckIndex == 9) {  selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex); }
-
-			// Insect
-			else if (DuelistMod.deckIndex == 10) {  selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(2); }
-
-			// Plant
-			else if (DuelistMod.deckIndex == 11) {  selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(2); }
-
-			else { selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(0); }
+			switch (DuelistMod.deckIndex) {
+				case 0: // Standard
+				case 1: // Dragon
+				case 2: // Naturia
+				case 3: // Spellcaster
+				case 4: // Toon
+				case 5: // Zombie
+				case 6: // Aqua
+				case 7: // Fiend
+				case 8: // Machine
+				case 9: // Warrior
+				case 10: // Insect
+				case 11: // Plant
+				case 13: // Megatype
+				case 16: // Ojama
+				case 17: // Exodia
+					selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(DuelistMod.deckIndex);
+					break;
+				default:
+					selectScreen.bgCharImg = TheDuelist.GetCharacterPortrait(0);
+			}
 		}
 	}
 
