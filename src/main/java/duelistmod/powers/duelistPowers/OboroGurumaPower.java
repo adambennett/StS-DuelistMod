8c6bbf32c495f4fe1774ee9e08ada9d49a26e0fa
May Lau <ascriptmaster@gmail.com>
2023-04-21 23:00:26 -0700

    Another Power Art Update

--- src/main/java/duelistmod/powers/duelistPowers/OboroGurumaPower.java
+++ src/main/java/duelistmod/powers/duelistPowers/OboroGurumaPower.java
@@ -18,7 +18,7 @@ public class OboroGurumaPower extends DuelistPower
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    public static final String IMG = DuelistMod.makePowerPath("PlaceholderPower.png");
+    public static final String IMG = DuelistMod.makePowerPath("OboroGurumaPower.png");
 	
 	public OboroGurumaPower(int turns) 
 	{ 
--- src/main/java/duelistmod/powers/duelistPowers/OverpoweringEyePower.java
+++ src/main/java/duelistmod/powers/duelistPowers/OverpoweringEyePower.java
@@ -17,7 +17,7 @@ public class OverpoweringEyePower extends NoStackDuelistPower
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    public static final String IMG = DuelistMod.makePowerPath("PlaceholderPower.png");
+    public static final String IMG = DuelistMod.makePowerPath("OverpoweringEyePower.png");
 	
 	public OverpoweringEyePower() 
 	{ 
