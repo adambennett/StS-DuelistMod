c3b0e21464dbb118b24c58b30ec2dcb9ab202257
May Lau <ascriptmaster@gmail.com>
2025-01-11 20:14:08 -0800

    More toon art + old arts I forgot to commit

--- src/main/java/duelistmod/powers/duelistPowers/DarkBribePower.java
+++ src/main/java/duelistmod/powers/duelistPowers/DarkBribePower.java
@@ -16,7 +16,7 @@ public class DarkBribePower extends DuelistPower {
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
-    public static final String IMG = DuelistMod.makePowerPath("PlaceholderPower.png");
+    public static final String IMG = DuelistMod.makePowerPath("DarkBribePower.png");
     private final AnyDuelist duelist;
 
 	public DarkBribePower(AbstractCreature owner, AbstractCreature source, int amount) {
